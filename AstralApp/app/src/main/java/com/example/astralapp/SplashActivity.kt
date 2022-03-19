e3dd8a98f362eb5fed98e7c5d4ec4e2e9b475075
Andrea <andiee.grr@gmail.com>
2022-03-19 01:23:53 -0600

    Add splash screen

---
+++ AstralApp/app/src/main/java/com/example/astralapp/SplashActivity.kt
@@ -0,0 +1,80 @@
+package com.example.astralapp
+
+import android.graphics.Color
+import android.graphics.LinearGradient
+import android.graphics.Shader
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.ViewAnimationUtils
+import androidx.core.animation.doOnEnd
+import com.example.astralapp.databinding.ActivitySplashBinding
+import kotlin.math.hypot
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.root.addOnLayoutChangeListener(object : View.OnLayoutChangeListener {
+            override fun onLayoutChange(
+                v: View,
+                left: Int,
+                top: Int,
+                right: Int,
+                bottom: Int,
+                oldLeft: Int,
+                oldTop: Int,
+                oldRight: Int,
+                oldBottom: Int
+            ) {
+                v.removeOnLayoutChangeListener(this)
+                animateView()
+            }
+        })
+        applyFontGradient()
+    }
+
+    private fun applyFontGradient() {
+        val paint = binding.textView.paint
+        val width = paint.measureText(binding.textView.text.toString())
+        val textShader: Shader = LinearGradient(
+            0f,
+            0f,
+            width,
+            binding.textView.textSize, intArrayOf(
+                Color.parseColor(getString(R.string.gradient_start)),
+                Color.parseColor(getString(R.string.gradient_mid)),
+                Color.parseColor(getString(R.string.gradient_end))
+            ),
+            null,
+            Shader.TileMode.REPEAT
+        )
+
+        binding.textView.paint.shader = textShader
+    }
+
+    private fun animateView() {
+        with(binding) {
+            val cx = logoImageView.width / 2
+            val cy = logoImageView.height / 2
+            val finalRadius = hypot(cx.toDouble(), cy.toDouble()).toFloat()
+            val anim = ViewAnimationUtils.createCircularReveal(
+                logoImageView,
+                cx,
+                cy,
+                0f, finalRadius
+            )
+            logoImageView.visibility = View.VISIBLE
+            anim.duration = 2000
+            anim.doOnEnd {
+
+            }
+            anim.start()
+
+        }
+    }
+}
